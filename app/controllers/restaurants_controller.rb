b616008949272eebf56cca1ba4ff52b480817ce8
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-22 16:37:42 +0200

    need to reboot

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,37 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
+    @restuarant = Restaurant.all
   end
 
   def show
+    find_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  def edit
+    find_restaurant
   end
 
   def update
+    find_restaurant
+    @restaurant.update(restaurant_params)
+    redirect_to @restaurant
   end
 
   def destroy
+    find_restaurant
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'restaurants/show'
   get 'restaurants/update'
   get 'restaurants/destroy'
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
