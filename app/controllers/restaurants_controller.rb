9716db4ebb17cb2356f393b1180132b4d095f63b
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-22 17:01:42 +0200

    added view files for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -34,4 +34,14 @@ class RestaurantsController < ApplicationController
     @restaurant.destroy
     redirect_to restaurants_path
   end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
