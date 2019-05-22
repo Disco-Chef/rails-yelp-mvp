5434f1bfcab63de151ebbb8cdbba8e023b2100cb
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-22 16:09:18 +0200

    added restaurant model and added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/create'
+  get 'restaurants/new'
+  get 'restaurants/edit'
+  get 'restaurants/show'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
